919001dc51bcbaa9804c0da23ffebe47a341bb39
Amy <amy.tai0120@gmail.com>
2018-01-17 22:17:07 +0000

    Q5 mod

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,7 @@
 #include <stdio.h>
-/**
- *
- *this is a comment
- *
- *
-**/
 
 int main(void)
 {
-	printf ("with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
