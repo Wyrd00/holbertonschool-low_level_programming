e91774dd33155ed37f98e0d1cdc1722f511f7eaa
Amy <amy.tai0120@gmail.com>
2018-01-18 21:56:23 +0000

    Q8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry block
+ * @void: no argument
+ * Return: 0 (Sucess)
+ **/
+int main(void)
+{
+	char alpha;
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+	for (alpha = 'a'; alpha <= 'f'; alpha++)
+	{
+		putchar(alpha);
+	}
+	putchar('\n');
+	return (0);
+}
