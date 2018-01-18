c2b78b88c5974ff02da18292b2269452d77db025
Amy <amy.tai0120@gmail.com>
2018-01-18 21:07:11 +0000

    Q4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry block
+ * @void: no argument
+ * Return: 0 (Sucess)
+ **/
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		if (alpha == 'e' || alpha == 'q')
+		{
+			alpha++;
+		}
+		putchar(alpha);
+	}
+	putchar('\n');
+	return (0);
+}
