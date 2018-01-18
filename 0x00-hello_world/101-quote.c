45852ae5e52756bdec4116e63c858c6412946f23
Amy <amy.tai0120@gmail.com>
2018-01-18 02:15:14 +0000

    Q101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	char str[]="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(2, str, 60);
-	return(1);
+	char s[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, s, 60);
+	return (1);
 }
