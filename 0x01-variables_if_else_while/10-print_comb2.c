74eb1139c3c26dbeb9689e1bcd0517851d1700ae
Amy <amy.tai0120@gmail.com>
2018-01-19 05:39:03 +0000

    Q10 finally

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - entry block
+ * @void: no argument
+ * Return: 0 (Sucess)
+ **/
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 99; n++)
+	{
+		putchar(n / 10 % 10 + '0');
+		putchar(n % 10 + '0');
+
+		if (n < 99)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
