ad4436af9bd0dc737905d7a33ddecd1ffdf1593f
Amy <amy.tai0120@gmail.com>
2018-01-20 01:37:12 +0000

    Q100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - entry block
+ * @void: no argument
+ * Return: 0 (Sucess)
+ **/
+int main(void)
+{
+	int n;
+	int m;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+       		for (m = '0'; m <= '9'; m++)
+		{
+			if (m > n)
+			{
+				putchar (n);
+				putchar (m);
+				if (!((m == '9') && (n == '8')))
+				{
+					putchar (',');
+					putchar (' ');
+				}
+			}
+		}
+	}
+	putchar ('\n');
+	return (0);
+}
