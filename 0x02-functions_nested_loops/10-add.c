33e32d030481a33e0fd3cfd5a24925a14a0f013f
Amy <amy.tai0120@gmail.com>
2018-01-24 01:06:50 +0000

    Q10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -4,13 +4,9 @@
  * add - add two ints together
  * @a: first int
  * @b: second int
- * @c: third int
  * Return: c
  */
 int add(int a, int b)
 {
-	int c;
-
-	c = a + b;
-	return (c);
+	return (a + b);
 }
