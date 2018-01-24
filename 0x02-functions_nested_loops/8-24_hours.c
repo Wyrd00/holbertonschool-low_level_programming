b0e6e4f8a1e0b4c49c0a6bee8548f1f536619dc6
Amy <amy.tai0120@gmail.com>
2018-01-24 02:20:32 +0000

    Q8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,18 +7,16 @@
  */
 void jack_bauer(void)
 {
-	int x, y, a, b;
+	int x, y;
 
-	for (x = 0; x <= 2; x++)
+	for (x = 0; x <= 23; x++)
 		for (y = 0; y <= 3; y++)
-			for (a = 0; a <= 5; a++)
-				for (b = 0; b <= 9; b++)
-				{
-					_putchar(x + '0');
-					_putchar(y + '0');
-					_putchar(':');
-					_putchar(a + '0');
-					_putchar(b + '0');
-					_putchar('\n');
-				}
+		{
+			_putchar(x / 10 + '0');
+			_putchar(x % 10 + '0');
+			_putchar(':');
+			_putchar(y / 10 + '0');
+			_putchar(y % 10 + '0');
+			_putchar('\n');
+		 }
 }
