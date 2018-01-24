a210d708f6c0006b096b235e52ed6f129081728a
Amy <amy.tai0120@gmail.com>
2018-01-24 04:39:31 +0000

    Q9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+
+/**
+ * times_table - print multiplication table using _putchar
+ * @x: outer int
+ * @y: inner int
+ * @z: product
+ * Return: multiplication table
+ */
+void times_table(void)
+{
+	int x, y, z;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = 0; y <= 9; y++)
+		{
+			z = x * y;
+			if ((y > 0) && (z < 10))
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(z % 10 + '0');
+			}
+			else if (z >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(z / 10 + '0');
+				_putchar(z % 10 + '0');
+			}
+			if (y == 0)
+			{
+				_putchar(z % 10 + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
