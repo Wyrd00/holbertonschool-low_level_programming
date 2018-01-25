fceb987f8927876794405fa5ebffc8c8f2700760
Amy <amy.tai0120@gmail.com>
2018-01-25 23:08:10 +0000

    Q7 mod

--- 0x03-more_functions_nested_loops/7-print_diagonal.c
+++ 0x03-more_functions_nested_loops/7-print_diagonal.c
@@ -19,8 +19,8 @@ void print_diagonal(int n)
 				_putchar(' ');
 			}
 		}
-                _putchar('\\');
-                _putchar('\n');
+		_putchar('\\');
+		_putchar('\n');
 	}
 	if (n <= 0)
 		_putchar('\n');
