eac54d355703e2008bf159f7ac5471d63172cfb0
Amy <amy.tai0120@gmail.com>
2018-01-30 19:17:08 +0000

    initial commit to readme, Q0 and Q1

---
+++ 0x04-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+ * reset_to_98 - takes an int pointer and update value to 98
+ * @*n: pointer to 98
+ * Result: return value of n
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x04-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * swap_int - swap value of two integers
+ * @*a: pointer of int
+ * @*b: pointer of int
+ * Result: swapped values
+ */
+void swap_int(int *a, int *b)
+{
+	int c = *a;
+	*a = *b;
+	*b = c;
+}
