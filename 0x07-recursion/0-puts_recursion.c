729f0098a6692a1327c7e34d6b908a65c0d5e924
Amy <amy.tai0120@gmail.com>
2018-02-08 03:57:43 +0000

    Q0 fix

--- 0x07-recursion/0-puts_recursion.c
+++ 0x07-recursion/0-puts_recursion.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+
 /**
  * _puts_recursion - print string, follow by newline
  * @s: string
@@ -6,12 +7,12 @@
 
 void _puts_recursion(char *s)
 {
-	int i = 0;
-
-	_putchar(s[i]);
-	i++;
-	if (s[i] != '\0')
-		_puts_recursion(s + i);
-	if (s[i] == '\0')
+	if (*s == 0)
+	{
 		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
 }
