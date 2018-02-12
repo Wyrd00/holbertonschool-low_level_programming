edc526a2800ca1a69255d772b0be79d1b9af72d7
Amy <amy.tai0120@gmail.com>
2018-02-12 07:14:30 +0000

    Q4

---
+++ 0x09-argc_argv/4-add.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * digit_check - check that the input are numbers
+ * @argc: size of argv
+ * @argv: array of size argc
+ * Return: 0 for success, 1 otherwise
+ */
+
+int digit_check(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		if (!(s[i] >= '0' && s[i] <= '9'))
+			return (-1);
+	return (0);
+}
+
+/**
+ * main - print number of arg
+ * @argc: size of argv
+ * @argv: array of size argc
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	int i;
+
+	for (i = 1; i < argc; i++)
+	{
+		if (argc == 1)
+		{
+			printf("0\n");
+			return (0);
+		}
+		else if (digit_check(argv[i]) == -1)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+			sum += atoi(argv[i]);
+
+	}
+	printf("%d\n", sum);
+	return (0);
+
+}
