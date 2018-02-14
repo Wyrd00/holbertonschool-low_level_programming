83e5cb119793d73e008cd28ae97e23afac9cb88c
Amy <amy.tai0120@gmail.com>
2018-02-14 03:03:36 +0000

    fix little cases

--- 0x0A-malloc_free/2-str_concat.c
+++ 0x0A-malloc_free/2-str_concat.c
@@ -28,21 +28,19 @@ int _strlen(char *str)
 char *str_concat(char *s1, char *s2)
 {
 	char *s3;
-	int i, j;
-
-	s3 = malloc(sizeof(char) * _strlen(s1) + _strlen(s2) + 1);
+	int i = 0;
+	int j;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
+
+        s3 = malloc(sizeof(char) * _strlen(s1) + _strlen(s2) + 1);
+
 	if (s3 == NULL)
 		return (NULL);
-	if (*s1 + *s2 == 0)
-	{
-		s3 = 0;
-		return (s3);
-	}
+
 	for (i = 0; s1[i] != '\0'; i++)
 		s3[i] = s1[i];
 	for (j = 0; s2[j] != '\0'; j++)
@@ -50,6 +48,6 @@ char *str_concat(char *s1, char *s2)
 		s3[i] = s2[j];
 		i++;
 	}
-	s3[i] += '\0';
+	s3[i] = '\0';
 	return (s3);
 }
