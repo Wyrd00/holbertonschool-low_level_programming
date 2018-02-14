0052d90208da9f218d5963bc1e136bd1ca6db6d8
Amy <amy.tai0120@gmail.com>
2018-02-14 04:12:15 +0000

    deleted and rety

--- 0x0A-malloc_free/1-strdup.c
+++ 0x0A-malloc_free/1-strdup.c
@@ -37,9 +37,7 @@ char *_strdup(char *str)
 	dup_str = malloc(sizeof(char) * len + 1);
 
 	if (dup_str == NULL)
-	{
 		return (NULL);
-	}
 	for (i = 0; i <= len; i++)
 		dup_str[i] = str[i]; /*Will copy null terminator as well*/
 	return (dup_str);
