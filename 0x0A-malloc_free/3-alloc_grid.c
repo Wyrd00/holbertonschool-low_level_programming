961980e4d9f4d8d3c0faf0ebf0b9b2db66bcc153
Amy <amy.tai0120@gmail.com>
2018-02-14 00:56:18 +0000

    free up the memory for if jiggy is up in the middle

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -15,14 +15,26 @@ int **alloc_grid(int width, int height)
 	int **arr2D;
 	int i, j;
 
-	arr2D = malloc(height * sizeof(int *));
-
-	if (width <= 0 || height <= 0 || arr2D == NULL)
+	if (width <= 0 || height <= 0)
 		return (NULL);
 
+	arr2D = malloc(height * sizeof(int *));
+
+	if (arr2D == NULL)
+		return (NULL);
 	for (i = 0; i < height; i++)
 	{
 		arr2D[i] = malloc(width * sizeof(int));
+
+		if (arr2D[i] == NULL)
+		{
+			while (i >= 0)
+			{
+				free(arr2D[--i]);
+			}
+			free(arr2D);
+			return (NULL);
+		}
 		for (j = 0; j < width; j++)
 			arr2D[i][j] = 0;
 	}
