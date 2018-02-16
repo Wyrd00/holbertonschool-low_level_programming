20d35de40f13a77d69a26e192f7779b6d3a66def
Amy <amy.tai0120@gmail.com>
2018-02-16 23:25:38 +0000

    tweaked

--- 0x0B-more_malloc_free/100-realloc.c
+++ 0x0B-more_malloc_free/100-realloc.c
@@ -7,7 +7,7 @@
  * @ptr: pointer to memory
  * @old_size: size of the old allocated space
  * @new_size: size of the new allocated space
- * Return:
+ * Return: new pointer
  */
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
@@ -16,16 +16,16 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	unsigned int i = 0;
 
 	if (ptr == NULL)
-		return (NULL);
+		return (malloc(new_size));
 	if (new_size == old_size)
 		return (ptr);
-	if (new_size == 0)
+	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
-	p = malloc(new_size + 1);
-	for (i = 0; i < new_size; i++)
+	p = malloc(new_size);
+	for (i = 0; i < new_size && i < old_size; i++)
 		p[i] = ((char *)ptr)[i];
 	return (p);
 }
