808f2baa427db630a7f5910c04238b9a1039cdd4
Amy <amy.tai0120@gmail.com>
2018-02-20 19:11:35 +0000

    omg i forgot my null terminator

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -15,43 +15,37 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *dawg;
 	int len_n, len_o;
 
-	dawg = malloc(sizeof(struct dog));
+	dawg = malloc(sizeof(dawg));
 	if (dawg == NULL)
 		return (NULL);
-
-	if (name != NULL)
-	{
-		for (len_n = 0; name[len_n] != '\0'; len_n++)
-			;
-		dawg->name = malloc(sizeof(char) * (len_n + 1));
-		if (dawg->name == NULL)
-			return (NULL);
-		for (len_n = 0; name[len_n] != '\0'; len_n++)
-			dawg->name[len_n] = name[len_n];
-	}
-	else
+	if (name == NULL)
 	{
 		free(dawg);
 		return (NULL);
 	}
-
+	for (len_n = 0; name[len_n] != '\0'; len_n++)
+			;
+	dawg->name = malloc(sizeof(char) * (len_n + 1));
+	if (dawg->name == NULL)
+		return (NULL);
+	for (len_n = 0; name[len_n] != '\0'; len_n++)
+		dawg->name[len_n] = name[len_n];
+	dawg->name[len_n] = '\0';
 	dawg->age = age;
 
-	if (owner != NULL)
-	{
-		for (len_o = 0; owner[len_o] != '\0'; len_o++)
-			;
-		dawg->owner = malloc(sizeof(char) * (len_o + 1));
-		if (dawg->owner == NULL)
-			return (NULL);
-		for (len_o = 0; owner[len_o] != '\0'; len_o++)
-			dawg->owner[len_o] = owner[len_o];
-	}
-	else
+	if (owner == NULL)
 	{
 		free(dawg->name);
 		free(dawg);
 		return (NULL);
 	}
+	for (len_o = 0; owner[len_o] != '\0'; len_o++)
+		;
+	dawg->owner = malloc(sizeof(char) * (len_o + 1));
+	if (dawg->owner == NULL)
+		return (NULL);
+	for (len_o = 0; owner[len_o] != '\0'; len_o++)
+		dawg->owner[len_o] = owner[len_o];
+	dawg->owner[len_o] = '\0';
 	return (dawg);
 }
