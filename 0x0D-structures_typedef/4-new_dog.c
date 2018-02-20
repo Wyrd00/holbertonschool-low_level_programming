2981cfdd3a17303ce758ecc4e89ab322586bb844
Amy <amy.tai0120@gmail.com>
2018-02-20 22:57:30 +0000

    change free to dawg->members

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -16,34 +16,41 @@ dog_t *new_dog(char *name, float age, char *owner)
 	int len_n, len_o;
 
 	dawg = malloc(sizeof(dog_t));
+
 	if (dawg == NULL)
 		return (NULL);
+
 	if (name == NULL)
+	{
+        	return (NULL);
+	}
+	for (len_n = 0; name[len_n] != '\0'; len_n++)
+		;
+	dawg->name = malloc(sizeof(char) * (len_n + 1));
+	if (dawg->name == NULL)
 	{
 		free(dawg);
 		return (NULL);
 	}
 	for (len_n = 0; name[len_n] != '\0'; len_n++)
-			;
-	dawg->name = malloc(sizeof(char) * (len_n + 1));
-	if (dawg->name == NULL)
-		return (NULL);
-	for (len_n = 0; name[len_n] != '\0'; len_n++)
 		dawg->name[len_n] = name[len_n];
 	dawg->name[len_n] = '\0';
+
 	dawg->age = age;
 
 	if (owner == NULL)
 	{
-		free(dawg->name);
-		free(dawg);
-		return (NULL);
+        	return (NULL);
 	}
 	for (len_o = 0; owner[len_o] != '\0'; len_o++)
 		;
 	dawg->owner = malloc(sizeof(char) * (len_o + 1));
 	if (dawg->owner == NULL)
+	{
+		free(dawg->name);
+		free(dawg);
 		return (NULL);
+	}
 	for (len_o = 0; owner[len_o] != '\0'; len_o++)
 		dawg->owner[len_o] = owner[len_o];
 	dawg->owner[len_o] = '\0';
