6d569aba1e1c92631fbd6ef65720a189563b3d4c
Amy <amy.tai0120@gmail.com>
2018-02-20 03:15:16 +0000

    Q4

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -13,11 +13,25 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dawg;
+	int len_n, len_o;
 
+	dawg = malloc(sizeof(struct dog));
 	if (dawg == NULL)
 		return (NULL);
-	dawg->name = name;
+	for (len_n = 0; name[len_n] != '\0'; len_n++)
+		;
+	dawg->name = malloc(len_n + 1);
+	if (dawg->name == NULL)
+		return (NULL);
+	for (len_n = 0; name[len_n] != '\0'; len_n++)
+		dawg->name[len_n] = name[len_n];
 	dawg->age = age;
-	dawg->owner = owner;
+	for (len_o = 0; owner[len_o] != '\0'; len_o++)
+		;
+	dawg->owner = malloc(len_o + 1);
+	if (dawg->owner == NULL)
+		return (NULL);
+	for (len_o = 0; owner[len_o] != '\0'; len_o++)
+		dawg->owner[len_o] = owner[len_o];
 	return (dawg);
 }
