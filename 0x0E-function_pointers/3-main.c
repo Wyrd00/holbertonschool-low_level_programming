fd7e8fadb97a7c98da50605b9037b6a7e0040478
Amy <amy.tai0120@gmail.com>
2018-02-22 00:16:16 +0000

    oh boy, here I go with 3

---
+++ 0x0E-function_pointers/3-main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "function_pointers.h"
+#include <stdlib.h>
+#include "3-calc.h"
+
+int main(int argc, char *argv[])
+{
+	int result, num1, num2;
+	int (*fun)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return (98);
+	}
+	fun = get_op_func(argv[2]);
+
+	if (fun == 0)
+	{
+		printf("Error\n");
+		return (99);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+
+	if ((fun == op_div || fun == op_mod) && num2 == 0)
+	{
+		printf("Error\n");
+		return (100);
+	}
+	result = fun(num1, num2);
+
+	printf("%d\n", result);
+	return (0);
+}
---
+++ 0x0E-function_pointers/3-op_functions.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include "function_pointers.h"
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * op_add - add two int
+ * @a: first int
+ * @b: second int
+ * Return: sum
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtract two int
+ * @a: first int
+ * @b: second int
+ * Return: difference
+ */
+
+int op_sub(int a, int b)
+{
+	return (b - a);
+}
+
+/**
+ * op_mult - multiply two int
+ * @a: first int
+ * @b: second int
+ * Return: product
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divide two int
+ * @a: first int
+ * @b: second int
+ * Return: division result
+ */
+
+int op_div(int a, int b)
+{
+	return (b / a);
+}
+
+/**
+ * op_mod - remainder of division of two int
+ * @a: first int
+ * @b: second int
+ * Return: remainder
+ */
+
+int op_mod(int a, int b)
+{
+	return (b % a);
+}
