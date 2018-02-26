25c06161d8204ef3dc54dda0a1619300732a7614
Amy <amy.tai0120@gmail.com>
2018-02-26 06:44:40 +0000

    haha sorry duh

--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -8,4 +8,15 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
+/**
+ * struct compare - struct
+ * @s: char
+ * @f: function pointer to the list
+ */
+typedef struct compare
+{
+	char *s;
+	void (*f)(va_list);
+} Compare;
+
 #endif
