3bc4deab99e5a6d0a1960b41d4b85a551ea44da7
Amy <amy.tai0120@gmail.com>
2018-03-02 01:41:26 +0000

    free?

--- 0x11-singly_linked_lists/3-add_node_end.c
+++ 0x11-singly_linked_lists/3-add_node_end.c
@@ -34,9 +34,12 @@ list_t *add_node_end(list_t **head, const char *str)
 
 	new_node = malloc(sizeof(list_t));
 
-	if (*head->str == NULL)
-		return (NULL);
 	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
 	new_node->len = _strlen(str);
 	new_node->next = NULL;
 
