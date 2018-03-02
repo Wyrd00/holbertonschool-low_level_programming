0b58cf58f0afdfac79e5b4c175eb13cae6c6b744
Amy <amy.tai0120@gmail.com>
2018-03-02 01:47:03 +0000

    new node fail

--- 0x11-singly_linked_lists/3-add_node_end.c
+++ 0x11-singly_linked_lists/3-add_node_end.c
@@ -34,12 +34,9 @@ list_t *add_node_end(list_t **head, const char *str)
 
 	new_node = malloc(sizeof(list_t));
 
-	new_node->str = strdup(str);
-	if (new_node->str == NULL)
-	{
-		free(new_node);
+        if (new_node == NULL)
 		return (NULL);
-	}
+	new_node->str = strdup(str);
 	new_node->len = _strlen(str);
 	new_node->next = NULL;
 
