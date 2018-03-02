911a03ee49ef849741a092581e2e8464c63022e4
Amy <amy.tai0120@gmail.com>
2018-03-02 01:32:52 +0000

    tried to see if str is null

--- 0x11-singly_linked_lists/3-add_node_end.c
+++ 0x11-singly_linked_lists/3-add_node_end.c
@@ -3,7 +3,7 @@
 
 /**
  * _strlen - get string length
- * @
+ * @s: string
  * Return: len
  */
 
@@ -37,6 +37,8 @@ list_t *add_node_end(list_t **head, const char *str)
 	new_node->len = _strlen(str);
 	new_node->next = NULL;
 
+	if (str == NULL)
+		return (NULL);
 	if (*head == NULL)
 	{
 		*head = new_node;
