15f83a5c240e6d258d4e9e5050815cceaca7570f
Amy <amy.tai0120@gmail.com>
2018-03-07 01:04:17 +0000

    Q9

---
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,48 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - insert new node at given posiition
+ * @head: pointer to beginning of list
+ * @idx: index of list where new node should be added
+ * @n: value of new node
+ * Return: address of new node at index idx, otherwise NULL
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *new_node;
+	listint_t *vagabond;
+	unsigned int current_i = 0;
+
+	if (head == NULL)
+		return (NULL);
+
+	new_node = malloc(sizeof(listint_t));
+
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+
+	vagabond = *head;
+	/*if my list is empty, but my idx got shiet*/
+	if (vagabond == NULL && idx > 0)
+	{
+		*head = new_node;
+		new_node->next = NULL;
+		return (new_node);
+	}
+	/*list not empty and node need to be in beginning*/
+	if (idx == 0)
+	{
+		vagabond = vagabond->next;
+		current_i++;
+	}
+	while (current_i != (idx - 1))
+	{
+		vagabond = vagabond->next;
+		current_i++;
+	}
+	new_node->next = vagabond->next;
+	vagabond->next = new_node;
+	return (new_node);
+}
