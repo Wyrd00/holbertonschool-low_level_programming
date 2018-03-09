6d9e52b954fc454508e4fa57354d744e7957482d
Amy <amy.tai0120@gmail.com>
2018-03-09 04:18:56 +0000

    added if statement to check that index is not greater than 8 bits

--- 0x13-bit_manipulation/2-get_bit.c
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -9,6 +9,9 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int num = 1;
 
+	if (index > sizeof(*n) * 8)
+		return (-1);
+
 	num <<= index;
 	if (num & n)
 		return (1);
--- 0x13-bit_manipulation/3-set_bit.c
+++ 0x13-bit_manipulation/3-set_bit.c
@@ -11,6 +11,8 @@ int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int num = 1;
 
+	if (index > sizeof(*n) * 8)
+		return (-1);
 	num <<= index;
 	*n = (*n | num);
 	return (1);
