9f67114abc60c0fd16a1fcab4059c0e9ddeab8e8
Amy <amy.tai0120@gmail.com>
2018-04-15 20:39:39 +0000

    betty argh

--- 0x16-doubly_linked_lists/6-sum_dlistint.c
+++ 0x16-doubly_linked_lists/6-sum_dlistint.c
@@ -1,23 +1,23 @@
 #include "lists.h"
 /**
- * sum_dlistint - 
- * @head: 
- * Return: 
+ * sum_dlistint - add all int of nodes together
+ * @head: list
+ * Return: sum of list
  */
 int sum_dlistint(dlistint_t *head)
 {
-        dlistint_t *vagabond;
-        int total;
+	dlistint_t *vagabond;
+	int total;
 
-        total = 0;
-        if (head == NULL)
-                return (0);
+	total = 0;
+	if (head == NULL)
+		return (0);
 
-        vagabond = head;
-        while (vagabond != NULL)
-        {
-                total += vagabond->n;
-                vagabond = vagabond->next;
-        }
-        return (total);
+	vagabond = head;
+	while (vagabond != NULL)
+	{
+		total += vagabond->n;
+		vagabond = vagabond->next;
+	}
+	return (total);
 }
