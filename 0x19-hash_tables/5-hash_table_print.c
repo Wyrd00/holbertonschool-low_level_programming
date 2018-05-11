3d018d74cfbd3edd23d9129dbc8c4c55637f1814
Amy <amy.tai0120@gmail.com>
2018-05-11 23:17:50 +0000

    initial commit

---
+++ 0x19-hash_tables/5-hash_table_print.c
@@ -0,0 +1,37 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - print the hash table
+ * @ht: table
+ * Return - printed table
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *vagabond;
+	unsigned int hiya;
+
+	if (!ht)
+		return;
+
+	i = 0;
+	vagabond = ht->array[i];
+	hiya = 0;
+
+	printf("{");
+	while (i < ht->size)
+	{
+		vagabond = ht->array[i];
+		while (vagabond)
+		{
+			if (hiya == 1)
+				printf(", ");
+			printf("'%s': '%s'", vagabond->key, vagabond->value);
+			hiya = 1;
+			vagabond = vagabond->next;
+		}
+		i++;
+	}
+	printf("}\n");
+}
