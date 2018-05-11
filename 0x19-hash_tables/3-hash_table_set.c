63532651b493c274c3487556944c3d6b5b8db0a9
Amy <amy.tai0120@gmail.com>
2018-05-11 23:17:39 +0000

    added const and other knicknacks

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -47,16 +47,16 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *vagabond;
 	hash_node_t *new;
 
-	if (ht == NULL || key == NULL || value == NULL)
+	if (!ht || !key || !value || !ht->array)
 	{
 		return (0);
 	}
 
-	index = key_index((unsigned char *)key, ht->size);
+	index = key_index((const unsigned char *)key, ht->size);
 	vagabond = ht->array[index];
 
 	/*check to see if key already exist*/
-	while (vagabond != NULL)
+	while (vagabond)
 	{
 		if (strcmp(vagabond->key, key) == 0)
 		{
