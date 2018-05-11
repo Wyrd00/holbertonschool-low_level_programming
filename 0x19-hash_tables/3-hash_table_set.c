bc907d04f63f1fa8cba97d1540636dda1f6f75f2
Amy <amy.tai0120@gmail.com>
2018-05-11 23:33:11 +0000

    meh

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -43,15 +43,13 @@ hash_node_t *create_node(const char *key, const char *value)
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index;
+	unsigned long int index = 0;
 	hash_node_t *vagabond;
 	hash_node_t *new;
 
 	if (!ht || !key || !value || !ht->array || ht->size == 0 ||
 	    strlen(key) == 0)
-	{
 		return (0);
-	}
 
 	index = key_index((const unsigned char *)key, ht->size);
 	vagabond = ht->array[index];
@@ -61,7 +59,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		if (strcmp(vagabond->key, key) == 0)
 		{
-			free(vagabond->value);
+			free(->value);
 			vagabond->value = strdup(value);
 			return (1);
 		}
@@ -72,6 +70,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (new == NULL)
 		return (0);
 
+	if (ht->array[index] == NULL)
+	{
+		ht->array[index] = new;
+		return (1);
+	}
 	/*move back to head of linked list and add node*/
 	vagabond = ht->array[index];
 	new->next = vagabond;
