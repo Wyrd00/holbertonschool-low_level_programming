8422dabddbab9be832991fef47153bf3b19221c8
Wyrd00 <amy.tai0120@gmail.com>
2018-07-06 21:02:37 +0000

    added parent null

--- 0x1C-binary_trees/1-binary_tree_insert_left.c
+++ 0x1C-binary_trees/1-binary_tree_insert_left.c
@@ -11,11 +11,14 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new, *temp;
 
+	if (!parent)
+		return (NULL);
+
 	new = malloc(sizeof(binary_tree_t));
 
 	if (!new)
 		return (NULL);
-	
+
 	new->n = value;
 	new->parent = parent;
 	new->left = NULL;
--- 0x1C-binary_trees/2-binary_tree_insert_right.c
+++ 0x1C-binary_trees/2-binary_tree_insert_right.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_node_insert_right - inserts a node as the right-child of another node
+ * binary_tree_node_insert_right - inserts node as right-child of another node
  * @parent: pointer to the node to insert the left-child in
  * @value: value to put in the new node
  * Return: return a pointer to the new node, or NULL on failure
@@ -11,11 +11,14 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new, *temp;
 
+	if (!parent)
+		return (NULL);
+
 	new = malloc(sizeof(binary_tree_t));
 
 	if (!new)
 		return (NULL);
-	
+
 	new->n = value;
 	new->parent = parent;
 	new->left = NULL;
