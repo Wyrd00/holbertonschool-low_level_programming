83584784c1c414471834cc6477bde862ad0e28fe
Amy <amy.tai0120@gmail.com>
2018-08-10 05:12:04 +0000

    what if i removed supp func?

--- 0x1D-search_algorithms/102-interpolation.c
+++ 0x1D-search_algorithms/102-interpolation.c
@@ -1,21 +1,6 @@
 #include "search_algos.h"
 
 /**
- * print_arr - print array
- * @array: array
- * @mid: mid
- * @lo: lo
- * @hi: high
- */
-
-void print_arr(int *array, size_t mid, size_t lo, size_t hi)
-{
-	if (mid <= hi && mid >= lo)
-		printf("Value checked array [%lu] = [%d]\n", mid, array[mid]);
-}
-
-
-/**
  * interpolation_search - search sorted array (but in an improved binary way)
  * @array: array to search through
  * @size: size of array
@@ -41,12 +26,12 @@ int interpolation_search(int *array, size_t size, int value)
 		mid = lo + (((double)(hi - lo) / (array[hi] - array[lo])) *
 			    (value - array[lo]));
 
-		print_arr(array, mid, lo, hi);
 		if (mid < lo || mid > hi)
 		{
 			printf("Value checked array[%lu] is out of range\n", mid);
 			break;
 		}
+		printf("Value checked array [%lu] = [%d]\n", mid, array[mid]);
 		if (value > array[mid])
 			lo = mid + 1;
 		else if (value < array[mid])
